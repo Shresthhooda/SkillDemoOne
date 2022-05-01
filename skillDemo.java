b88a814736ec2155a33beaa480db2591e1d6ffc9
Shresthhooda <shooda@ucsd.edu>
2022-04-30 21:16:16 -0700

    Failing test

--- skillDemo.java
+++ skillDemo.java
@@ -1,1 +1,6 @@
-
+public class skillDemo {
+    public static int subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ skillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void subtractTest() {
+        assertEquals(2, skillDemo.subtract(2, 1));
+    }
+
+}
